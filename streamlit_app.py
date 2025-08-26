894e52b14aa6dea3135f0d9730d56512a0f04120
Sara Almeida <sarakaliana@gmail.com>
2025-08-26 19:16:57 -0300

    Atualização do código

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,51 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("🎈 Meu primeiro APP")
+
+
+# Título do app
+st.title("Comparação de Indicadores Educacionais – MEPES x Escolas do Campo no ES")
+
+# Nome da autora
+st.write("Desenvolvido por **Sara Kaliana de Almeida Ferreira**")
+
+st.header("3. Filtros de Visualização")
+
+tipo_escola = st.selectbox(
+    "Selecione o tipo de escola:",
+    ["MEPES (EFAs)", "Escolas do Campo da Rede"]
 )
+
+etapa_ensino = st.selectbox(
+    "Selecione a etapa de ensino:",
+    ["Ensino Fundamental - Anos Iniciais", 
+     "Ensino Fundamental - Anos Finais", 
+     "Ensino Médio"]
+)
+
+indicador = st.radio(
+    "Selecione o indicador:",
+    ["Taxa de Abandono", "Distorção Idade-Série"]
+)
+
+st.write(f"📌 Você selecionou: **{tipo_escola}** | **{etapa_ensino}** | **{indicador}**")
+
+
+# Análises
+
+st.header("5. Análises e Insights")
+st.write("""
+Nesta seção serão apresentados os principais achados a partir das comparações,
+destacando o papel das EFAs e da pedagogia da alternância na permanência escolar.
+""")
+
+# Bases de Dados
+# ==============================
+st.header("2. Bases de Dados")
+st.write("""
+Os dados utilizados serão provenientes dos **Microdados do Censo Escolar (INEP)**,
+filtrando as escolas do campo e as escolas do MEPES no Espírito Santo.
+Os indicadores a serem analisados são:
+- Taxa de Abandono
+- Distorção Idade-Série
+""")
